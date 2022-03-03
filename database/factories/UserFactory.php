db24127d14271beb956496d59e21dceaf8c73568
Anastasiya Ostertag <nastasja83@mail.ru>
2022-03-03 18:55:58 +0700

    create Models factories for tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
+use App\Position;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -17,9 +18,15 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+
+    $position = Position::inRandomOrder()->first();
     return [
-        'name' => $faker->name,
+        'last_name' => $faker->lastName,
+        'first_name' => $faker->firstName,
+        'middle_name' => $faker->firstName,
+        'position_id' => $position->id,
         'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->unique()->regexify('/^(\+79)[0-9]{9}$/'),
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),

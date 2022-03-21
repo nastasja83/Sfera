75c759d59500c0aa02a22a000ba8443183924f81
Anastasiya Ostertag <nastasja83@mail.ru>
2022-03-21 21:31:57 +0700

    fix layout

--- resources/views/skills/edit.blade.php
+++ resources/views/skills/edit.blade.php
@@ -2,7 +2,9 @@
 <div class="input-group">
 {{Form::text('skill_name', $skill->skill_name, ['class' => 'form-control-plaintext', 'id' => 'input_skill'])}}
   <div class="input-group-append">
-    <label for="input_skill">{{ Form::button('<i class="bi bi-pencil-square"></i>', ['class' => 'btn btn-link disabled', 'type' => 'submit']) }}</label>
+    <label for="input_skill">
+        {{ Form::button('<i class="bi bi-pencil-square"></i>', ['class' => 'btn btn-link disabled', 'type' => 'submit']) }}
+    </label>
   </div>
 </div>
 {!!Form::close()!!}

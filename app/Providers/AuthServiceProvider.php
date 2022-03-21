91c8e85aa18ee23a241d9f8f7950d1e2258b7345
Anastasiya Ostertag <nastasja83@mail.ru>
2022-03-21 15:42:36 +0700

    register SkillPolicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use App\User;
+use App\Skill;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -16,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         User::class => UserPolicy::class,
+        Skill::class => SkillPolicy::class,
     ];
 
     /**

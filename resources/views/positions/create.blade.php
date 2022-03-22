a503997943e1d7fae3dc70b35db557e868a11106
Anastasiya Ostertag <nastasja83@mail.ru>
2022-03-22 18:36:10 +0700

    add positions.create.blade.php

---
+++ resources/views/positions/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <h1 class="mb-3 mt-3">{{ __('positions.Create position') }}</h1>
+            {{Form::open(['url' => route('positions.store'), 'class' => 'w-50'])}}
+                <div class="form-group mb-3">
+                    {{Form::label('position_name', __('positions.Position name'))}}
+                    {{Form::text('position_name', '', ['class' => 'form-control'])}}
+                    <div class="invalid-feedback d-block">
+                    @if ($errors->any())
+                        @foreach ($errors->all() as $error)
+                            {{ $error }}
+                        @endforeach
+                    @endif
+                    </div>
+                </div>
+                    {{Form::submit(__('positions.Create'), ['class' => 'btn btn-primary mt-3'])}}
+            {{ Form::close() }}
+        </div>
+    </div>
+</div>
+            @endsection('content')

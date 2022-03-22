fc1f5219a0b1a795b737fcaa867ba6cef39ca7c3
Anastasiya Ostertag <nastasja83@mail.ru>
2022-03-22 15:42:22 +0700

    add positions.action_buttons.blade.php

---
+++ resources/views/positions/action_buttons.blade.php
@@ -0,0 +1,4 @@
+
+{{ Form::button('Update', ['class' => 'btn btn-sm btn-primary', 'type' => 'submit', 'form' => 'position_update']) }}
+<a href="{{ route('positions.destroy', ['position' => $position->id]) }}" class="btn btn-outline-danger btn-sm" data-method="delete" rel="nofollow" data-confirm="{{ __('tasks.Are you sure?') }}" aria-pressed="true">Delete</a>
+

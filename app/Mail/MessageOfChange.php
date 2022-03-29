ad135f965a9abb8fac56d8b104a30eb93ea2a2d9
Anastasiya Ostertag <nastasja83@mail.ru>
2022-03-29 12:51:39 +0700

    create mailable class for message of change

---
+++ app/Mail/MessageOfChange.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class MessageOfChange extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $userChange;
+    public $skillsChange;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $userChange, $skillsChange)
+    {
+        $this->user = $user;
+        $this->userChange = $userChange;
+        $this->skillsChange = $skillsChange;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('mails.message_of_change')
+                    ->subject('Изменение в карточке сотрудника');
+    }
+}

3a475d42eef6aaf97ef871bbfc259d0b10da508f
Anastasiya Ostertag <nastasja83@mail.ru>
2022-04-01 17:50:49 +0700

    change isNotAdmin translation

--- resources/lang/ru/users.php
+++ resources/lang/ru/users.php
@@ -17,12 +17,12 @@ return [
     'Skills' => 'Навыки',
     'Position' => 'Должность',
     'isAdmin' => 'Администратор',
-    'isNotAdmin' => 'Удалить права администратора',
+    'isNotAdmin' => 'Сотрудник',
     'Employees' => 'Сотрудники',
     'Creating date' => 'Дата создания',
     'Actions' => 'Действия',
     'Online' => 'Онлайн',
     'User has been updated successfully' => 'Данные пользователя успешно обновлены',
-    'User has been deleted successfully' => 'Пользователь успешно удален'
+    'User has been deleted successfully' => 'Пользователь успешно удален',
 
 ];

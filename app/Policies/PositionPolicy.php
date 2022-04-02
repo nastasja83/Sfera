b43f4a3060553d8f351baf73bbe5c9ac857c0b97
Anastasiya Ostertag <nastasja83@mail.ru>
2022-04-02 19:40:18 +0700

    delete unused methods

--- app/Policies/PositionPolicy.php
+++ app/Policies/PositionPolicy.php
@@ -11,29 +11,6 @@ class PositionPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any positions.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(?User $user)
-    {
-        return true;
-    }
-
-    /**
-     * Determine whether the user can view the position.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Position  $position
-     * @return mixed
-     */
-    public function view(User $user, Position $position)
-    {
-        return true;
-    }
-
-    /**
      * Determine whether the user can create positions.
      *
      * @param  \App\User  $user
@@ -66,29 +43,5 @@ class PositionPolicy
     public function delete(User $user, Position $position)
     {
         return $user->isAdmin();
-    }
-
-    /**
-     * Determine whether the user can restore the position.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Position  $position
-     * @return mixed
-     */
-    public function restore(User $user, Position $position)
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the position.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Position  $position
-     * @return mixed
-     */
-    public function forceDelete(User $user, Position $position)
-    {
-        return false;
     }
 }
--- app/Policies/SkillPolicy.php
+++ app/Policies/SkillPolicy.php
@@ -11,29 +11,6 @@ class SkillPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any skills.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(?User $user)
-    {
-        return true;
-    }
-
-    /**
-     * Determine whether the user can view the skill.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Skill  $skill
-     * @return mixed
-     */
-    public function view(User $user, Skill $skill)
-    {
-        return true;
-    }
-
-    /**
      * Determine whether the user can create skills.
      *
      * @param  \App\User  $user
@@ -66,29 +43,5 @@ class SkillPolicy
     public function delete(User $user, Skill $skill)
     {
         return $user->isAdmin();
-    }
-
-    /**
-     * Determine whether the user can restore the skill.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Skill  $skill
-     * @return mixed
-     */
-    public function restore(User $user, Skill $skill)
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the skill.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Skill  $skill
-     * @return mixed
-     */
-    public function forceDelete(User $user, Skill $skill)
-    {
-        return false;
     }
 }
